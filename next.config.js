6dad8425833a5f7652ebfcc19c02c810c33ef474
SangND <dacsang97@gmail.com>
2018-02-17 13:27:57 +0700

    Update /docs to deploy in ghpages

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  exportPathMap: function() {
+    return {
+      "/": { page: "/" }
+    };
+  },
+  assetPrefix: process.env.NODE_ENV === "production" ? "/waiting-m2love" : ""
+};
