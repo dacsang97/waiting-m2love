4b878c2a9b92ef1c79779b012ec3c73ead5c5d2b
SangND <dacsang97@gmail.com>
2018-02-17 13:56:03 +0700

    Update assets path

--- next.config.js
+++ next.config.js
@@ -4,6 +4,5 @@ module.exports = {
       "/": { page: "/" }
     };
   },
-  assetPrefix:
-    process.env.NODE_ENV === "production" ? "/waiting-m2love/docs" : ""
+  assetPrefix: process.env.NODE_ENV === "production" ? "/waiting-m2love" : ""
 };
